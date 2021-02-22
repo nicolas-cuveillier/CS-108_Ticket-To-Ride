6f4cccc968b375ff1c0f189cfada3ea7c976c49a
Nicolas Cuveillier <Nicolas.cuveillier@epfl.ch>
2021-02-22 14:19:21 +0100

    Initial commit

---
+++ src/ch/epfl/tchu/Preconditions.java
@@ -0,0 +1,12 @@
+package ch.epfl.tchu;
+
+public final class Preconditions {
+
+    private Preconditions(){}
+
+    public static void checkArgument(boolean shouldBeTrue){
+        if(!shouldBeTrue){
+            throw new IllegalArgumentException();
+        }
+    }
+}
---
+++ src/ch/epfl/tchu/game/Color.java
@@ -0,0 +1,12 @@
+package ch.epfl.tchu.game;
+
+public enum Color {
+    BLACK ,
+    VIOLET,
+    BLUE ,
+    GREEN ,
+    YELLOW ,
+    ORANGE,
+    RED ,
+    WHITE,
+}
