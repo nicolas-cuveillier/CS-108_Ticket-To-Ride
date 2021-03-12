e6c0cd5b9dbe28ac859ab9481d4904fe257e9123
Chocolat2 <33324447+Chocolat2@users.noreply.github.com>
2021-03-12 15:58:10 +0100

    Create StationPartitionTest.java

---
+++ testEtape4/ch/epfl/tchu/game/StationPartitionTest.java
@@ -0,0 +1,14 @@
+package ch.epfl.tchu.game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StationPartitionTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
