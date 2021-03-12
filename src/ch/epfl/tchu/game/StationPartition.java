5bc446838537d2f72c51078ec31818d530bd8469
Chocolat2 <33324447+Chocolat2@users.noreply.github.com>
2021-03-12 14:35:44 +0100

    Create StationPartition.java
    Created StationPartition.java wich is implementing StationConnectivity

---
+++ src/ch/epfl/tchu/game/StationPartition.java
@@ -0,0 +1,11 @@
+package ch.epfl.tchu.game;
+
+public final class StationPartition implements StationConnectivity{
+
+    @Override
+    public boolean connected(Station st1, Station st2) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
