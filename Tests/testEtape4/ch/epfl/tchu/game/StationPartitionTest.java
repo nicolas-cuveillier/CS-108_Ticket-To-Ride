736b9e5cb78a02aaf0071d7f4d20ac4e39d32dab
Chocolat2 <33324447+Chocolat2@users.noreply.github.com>
2021-03-18 17:00:50 +0100

    Update StationPartitionTest.java
    Wrote StationPartition unit tests

--- Tests/testEtape4/ch/epfl/tchu/game/StationPartitionTest.java
+++ Tests/testEtape4/ch/epfl/tchu/game/StationPartitionTest.java
@@ -7,8 +7,27 @@ import org.junit.jupiter.api.Test;
 class StationPartitionTest {
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void notConnectedWithOutOfBoundsAndDifferentStations() {
+        StationPartition.Builder sp = new StationPartition.Builder(5);
+        assertFalse(sp.build().connected(ChMap.stations().get(6), ChMap.stations().get(7)));
     }
 
+    @Test
+    void notConnectedWithNotConnectedStations() {
+        StationPartition.Builder sp = new StationPartition.Builder(5);
+        assertFalse(sp.build().connected(ChMap.stations().get(0), ChMap.stations().get(1)));
+    }
+    
+    @Test
+    void connectedWithOutOfBoundsAndSameStations() {
+        StationPartition.Builder sp = new StationPartition.Builder(5);
+        assertTrue(sp.build().connected(ChMap.stations().get(6), ChMap.stations().get(6)));
+    }
+    
+    @Test
+    void connectedWithCorrectStations() {
+        StationPartition.Builder sp = new StationPartition.Builder(5);
+        sp.connect(ChMap.stations().get(0), ChMap.stations().get(1));
+        assertTrue(sp.build().connected(ChMap.stations().get(0), ChMap.stations().get(1)));
+    }
 }
