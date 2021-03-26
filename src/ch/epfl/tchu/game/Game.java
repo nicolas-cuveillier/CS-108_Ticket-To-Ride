e8262b047b33a3460c8514642d20ecc8fc1cf2f2
Nicolas Cuveillier <Nicolas.cuveillier@epfl.ch>
2021-03-26 15:23:56 +0100

    add class Game and started it

---
+++ src/ch/epfl/tchu/game/Game.java
@@ -0,0 +1,149 @@
+package ch.epfl.tchu.game;
+
+import ch.epfl.tchu.Preconditions;
+import ch.epfl.tchu.SortedBag;
+import ch.epfl.tchu.gui.Info;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * @author Grégory Preisig & Nicolas Cuveillier
+ */
+public final class Game {
+    private Game() {
+    }
+
+    public static void play(Map<PlayerId, Player> players, Map<PlayerId, String> playerNames, SortedBag<Ticket> tickets, Random rng) {
+        Preconditions.checkArgument(players.size() == PlayerId.COUNT && playerNames.size() == PlayerId.COUNT);
+        //TODO : use foreach, stream etc
+        //1.create GameState
+        GameState gameState = GameState.initial(tickets, rng);
+
+        //2.communicate names
+        players.get(gameState.currentPlayerId()).initPlayers(gameState.currentPlayerId(), playerNames);
+        players.get(gameState.currentPlayerId().next()).initPlayers(gameState.currentPlayerId().next(), playerNames);
+
+        Info currentPlayer = new Info(playerNames.get(gameState.currentPlayerId()));
+        Info nextPlayer = new Info(playerNames.get(gameState.currentPlayerId().next()));
+
+        //3. willPlayFirstInfo
+        for (Player p : players.values()) {
+            p.receiveInfo(currentPlayer.willPlayFirst());
+        }
+
+        //4.setInitialTicketChoice
+        players.get(gameState.currentPlayerId()).setInitialTicketChoice(gameState.topTickets(Constants.INITIAL_TICKETS_COUNT));
+        gameState = gameState.withoutTopTickets(Constants.INITIAL_TICKETS_COUNT);
+        players.get(gameState.currentPlayerId().next()).setInitialTicketChoice(gameState.topTickets(Constants.INITIAL_TICKETS_COUNT));
+        gameState = gameState.withoutTopTickets(Constants.INITIAL_TICKETS_COUNT);
+
+        //5.update state
+        players.get(gameState.currentPlayerId()).updateState(gameState, gameState.playerState(gameState.currentPlayerId()));
+        players.get(gameState.currentPlayerId().next()).updateState(gameState, gameState.playerState(gameState.currentPlayerId().next()));
+
+        //6.chooseInitialTickets
+        for (Player p : players.values()) {
+            p.chooseInitialTickets();
+        }
+
+        //7.receive info
+        for (Player p : players.values()) {
+            p.receiveInfo(currentPlayer.keptTickets(gameState.playerState(gameState.currentPlayerId()).ticketCount()));
+        }
+        for (Player p : players.values()) {
+            p.receiveInfo(nextPlayer.keptTickets(gameState.playerState(gameState.currentPlayerId().next()).ticketCount()));
+        }
+
+        boolean isPlaying = true;
+
+        while (isPlaying) {
+
+            //1.update state
+            players.get(gameState.currentPlayerId()).updateState(gameState, gameState.playerState(gameState.currentPlayerId()));
+            players.get(gameState.currentPlayerId().next()).updateState(gameState, gameState.playerState(gameState.currentPlayerId().next()));
+
+            //2. info
+            for (Player p : players.values()) {
+                p.receiveInfo(currentPlayer.canPlay());
+            }
+
+            //3.next turn
+            Player.TurnKind turn = players.get(gameState.currentPlayerId()).nextTurn();
+            //TODO : insert method ifDeckRecreatedIfNeeded
+            switch (turn) {
+
+                case DRAW_TICKETS:
+                    SortedBag<Ticket> chosenTickets = players.get(gameState.currentPlayerId()).chooseTickets(gameState.topTickets(Constants.IN_GAME_TICKETS_COUNT));
+                    gameState = gameState.withoutTopTickets(Constants.IN_GAME_TICKETS_COUNT);
+
+                    for (Player p : players.values()) {
+                        p.receiveInfo(currentPlayer.drewTickets(Constants.IN_GAME_TICKETS_COUNT));
+                        p.receiveInfo(currentPlayer.keptTickets(chosenTickets.size()));
+                    }
+
+                case DRAW_CARDS:
+                    gameState = gameState.withCardsDeckRecreatedIfNeeded(rng);
+                    int slot1 = players.get(gameState.currentPlayerId()).drawSlot();
+                    gameState = (slot1 == Constants.DECK_SLOT) ? gameState.withBlindlyDrawnCard() : gameState.withDrawnFaceUpCard(slot1);
+
+                    for (Player p : players.values()) {
+                        if ((slot1 == Constants.DECK_SLOT)) {
+                            p.receiveInfo(currentPlayer.drewBlindCard());
+                        } else {
+                            p.receiveInfo(currentPlayer.drewVisibleCard(gameState.cardState().faceUpCard(slot1)));
+                        }
+                    }
+
+                    players.get(gameState.currentPlayerId()).updateState(gameState, gameState.playerState(gameState.currentPlayerId()));
+                    players.get(gameState.currentPlayerId().next()).updateState(gameState, gameState.playerState(gameState.currentPlayerId().next()));
+
+                    gameState = gameState.withCardsDeckRecreatedIfNeeded(rng);
+                    int slot2 = players.get(gameState.currentPlayerId()).drawSlot();
+                    gameState = (slot2 == Constants.DECK_SLOT) ? gameState.withBlindlyDrawnCard() : gameState.withDrawnFaceUpCard(slot2);
+
+                    for (Player p : players.values()) {
+                        if ((slot2 == Constants.DECK_SLOT)) {
+                            p.receiveInfo(currentPlayer.drewBlindCard());
+                        } else {
+                            p.receiveInfo(currentPlayer.drewVisibleCard(gameState.cardState().faceUpCard(slot1)));
+                        }
+                    }
+
+                case CLAIM_ROUTE:
+
+                    Route claimRoute = players.get(gameState.currentPlayerId()).claimedRoute();
+                    SortedBag<Card> initialClaimCards = players.get(gameState.currentPlayerId()).initialClaimCards();
+
+                    if (claimRoute.level() == Route.Level.UNDERGROUND) {
+                        for (Player p : players.values()) {
+                            p.receiveInfo(currentPlayer.claimedRoute(claimRoute, initialClaimCards));
+                        }
+                    } else {
+                        for (Player p : players.values()) {
+                            p.receiveInfo(currentPlayer.attemptsTunnelClaim(claimRoute, initialClaimCards));
+                        }
+
+                        SortedBag.Builder drawnCards = new SortedBag.Builder();
+                        for (int i = 0; i < Constants.ADDITIONAL_TUNNEL_CARDS; i++) {
+                            gameState = gameState.withCardsDeckRecreatedIfNeeded(rng);
+                            drawnCards.add(gameState.topCard());
+                            gameState = gameState.withoutTopCard();
+                        }
+
+                        int additionalCards = claimRoute.additionalClaimCardsCount(initialClaimCards, drawnCards.build());
+                        List<SortedBag<Card>> possibleClaimCards = gameState.playerState(gameState.currentPlayerId()).possibleAdditionalCards(additionalCards,initialClaimCards,drawnCards.build());
+
+                        //TODO : que le joueur courant a dans sa main les cartes additionnelles nécessaires, alors :
+                        //la méthode chooseAdditionalCards est appelée pour déterminer s'il désire jouer des cartes additionnelles, et si oui, lesquelles.
+                        //TODO : receive info
+                        //TODO : end of the game
+                    }
+            }
+
+
+        }
+
+    }
+}
--- src/ch/epfl/tchu/game/Player.java
+++ src/ch/epfl/tchu/game/Player.java
@@ -7,10 +7,9 @@ import ch.epfl.tchu.SortedBag;
 
 /**
  * @author Grégory Preisig & Nicolas Cuveillier
- *
  */
 public interface Player {
-    
+
     /**
      * @author Grégory Preisig & Nicolas Cuveillier
      * <p>
@@ -19,77 +18,95 @@ public interface Player {
      * {@link #DRAW_CARDS}
      * {@link #CLAIM_ROUTE}
      */
-    enum TurnKind{
+    enum TurnKind {
         DRAW_TICKETS,
         DRAW_CARDS,
         CLAIM_ROUTE;
-        
+
         /**
          * @return a list of all the turn kinds
          */
-        public static final List<TurnKind> All(){
-            return List.of(DRAW_TICKETS,DRAW_CARDS,CLAIM_ROUTE);
+        public static final List<TurnKind> All() {
+            return List.of(DRAW_TICKETS, DRAW_CARDS, CLAIM_ROUTE);
         }
     }
-    
+
     /**
-     * @param ownId Id of the player (self)
+     * @param ownId       Id of the player (self)
      * @param playerNames Ids of all the payers, mapped to their name (includes self)
      */
     void initPlayers(PlayerId ownId, Map<PlayerId, String> playerNames);
-    
+
     /**
      * @param info Information to give to the player, parsed by the class Info
      */
     void receiveInfo(String info);
-    
-    /** Called each time the game state changes
+
+    /**
+     * Called each time the game state changes
+     *
      * @param newState new state of the game
      * @param ownState current state of the player
      */
     void updateState(PublicGameState newState, PlayerState ownState);
-    
-    /** Called during the initialization of the game to assign the player it's 5 tickets
+
+    /**
+     * Called during the initialization of the game to assign the player it's 5 tickets
+     *
      * @param tickets 5 initial tickets assigned to the player
      */
     void setInitialTicketChoice(SortedBag<Ticket> tickets);
-    
-    /** Called at the beginning of the game to get the initial tickets assigned to the player
+
+    /**
+     * Called at the beginning of the game to get the initial tickets assigned to the player
+     *
      * @return 5 initial tickets assigned to the player
      */
     SortedBag<Ticket> chooseInitialTickets();
-    
-    /** Called at the beginning of the player's turn
+
+    /**
+     * Called at the beginning of the player's turn
+     *
      * @return the action taken by the player during his turn
      */
     TurnKind nextTurn();
-    
-    /** Called when the player chooses to draw tickets during his turn
+
+    /**
+     * Called when the player chooses to draw tickets during his turn
+     *
      * @param options the tickets drawn by the player
      * @return the tickets kept by the player
      */
     SortedBag<Ticket> chooseTickets(SortedBag<Ticket> options);
-    
-    /** Called when the player chooses to draw cards during his turn
+
+    /**
+     * Called when the player chooses to draw cards during his turn
+     *
      * @return the slot's id from which the player drew a card (0-4 for visible cards or {@link Constants (DECK_SLOT)} for the deck)
      */
     int drawSlot();
-    
-    /** Called when the player chooses to try to claim a route during his turn
+
+    /**
+     * Called when the player chooses to try to claim a route during his turn
+     *
      * @return the route the player tries to claim
      */
     Route claimedRoute();
-    
-    /** Called when the player chooses to try to claim a route during his turn
+
+    /**
+     * Called when the player chooses to try to claim a route during his turn
+     *
      * @return the cards chosen by the player to try to claim the route
      */
     SortedBag<Card> initialClaimCards();
-    
-    /** Called when the player chooses to try to claim a tunnel during his turn
+
+    /**
+     * Called when the player chooses to try to claim a tunnel during his turn
+     *
      * @param options the additional cards to claim the tunnel
      * @return the additional cards chosen by the player, empty of the player refuses to spend the additional cards or cannot fulfill the requirement
      */
     SortedBag<Card> chooseAdditionalCards(List<SortedBag<Card>> options);
-    
-    
+
+
 }
