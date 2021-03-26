9e095e55c30ed8423265b663f3a3cfffd71dc800
Chocolat2 <33324447+Chocolat2@users.noreply.github.com>
2021-03-26 13:48:08 +0100

    Update Constants.java
    Translated the javadoc from french to english

--- src/ch/epfl/tchu/game/Constants.java
+++ src/ch/epfl/tchu/game/Constants.java
@@ -9,23 +9,23 @@ public final class Constants {
     }
 
     /**
-     * Nombre de cartes wagon de chaque couleur.
+     * Total number of wagon cards for each color.
      */
     public static final int CAR_CARDS_COUNT = 12;
 
     /**
-     * Nombre de cartes locomotive.
+     * Total number of locomotive cards.
      */
     public static final int LOCOMOTIVE_CARDS_COUNT = 14;
 
     /**
-     * Nombre total de cartes wagon/locomotive.
+     * Total number of cards.
      */
     public static final int TOTAL_CARDS_COUNT =
             LOCOMOTIVE_CARDS_COUNT + CAR_CARDS_COUNT * Color.COUNT;
 
     /**
-     * Ensemble de toutes les cartes (110 au total).
+     * Collection of all the cards (110 in total).
      */
     public static final SortedBag<Card> ALL_CARDS = computeAllCards();
 
@@ -39,70 +39,69 @@ public final class Constants {
     }
 
     /**
-     * Numéro d'emplacement fictif désignant la pioche de cartes.
+     * Id for the deck.
      */
     public static final int DECK_SLOT = -1;
 
     /**
-     * Liste de tous les numéros d'emplacements de cartes face visible.
+     * List of the ids of the visible card.
      */
     public static final List<Integer> FACE_UP_CARD_SLOTS = List.of(0, 1, 2, 3, 4);
 
     /**
-     * Nombre d'emplacements pour les cartes face visible.
+     * Number of slots for the visile cards.
      */
     public static final int FACE_UP_CARDS_COUNT = FACE_UP_CARD_SLOTS.size();
 
     /**
-     * Nombre de billets distribués à chaque joueur en début de partie.
+     * Number of tickets given to each player at the beginning of the game.
      */
     public static final int INITIAL_TICKETS_COUNT = 5;
 
     /**
-     * Nombre de cartes distribuées à chaque joueur en début de partie.
+     * Number of cards given to each player at the beginning of the game.
      */
     public static final int INITIAL_CARDS_COUNT = 4;
 
     /**
-     * Nombre de wagons dont dispose chaque joueur en début de partie.
+     * Number of wagons each player has at the beginning of the game.
      */
     public static final int INITIAL_CAR_COUNT = 40;
 
     /**
-     * Nombre de billets tirés à la fois en cours de partie.
+     * Number of tickets drawn each time during the game.
      */
     public static final int IN_GAME_TICKETS_COUNT = 3;
 
     /**
-     * Nombre maximum de billets qu'un joueur peut défausser lors d'un tirage.
+     * Maximum number of tickets a player can discard after a ticket draw.
      */
     public static final int DISCARDABLE_TICKETS_COUNT = 2;
 
     /**
-     * Nombre de cartes à tirer lors de la construction d'un tunnel.
+     * Number of cards to draw as additional cards to claim a tunnel.
      */
     public static final int ADDITIONAL_TUNNEL_CARDS = 3;
 
     /**
-     * Nombre de points obtenus pour la construction de routes de longueur 1 à 6.
-     * (L'élément à l'index i correspond à une longueur de route i. Une valeur
-     * invalide est placée à l'index 0, car les routes de longueur 0 n'existent pas).
+     * Number of points awarded for a claimed route of length i, 1<= i <= 6.
+     * the first element (index 0) has an invalid value since there are no routes with a length of 0.
      */
     public static final List<Integer> ROUTE_CLAIM_POINTS =
             List.of(Integer.MIN_VALUE, 1, 2, 4, 7, 10, 15);
 
     /**
-     * Longueur minimum d'une route.
+     * Minimum length of any route.
      */
     public static final int MIN_ROUTE_LENGTH = 1;
 
     /**
-     * Longueur maximum d'une route.
+     * Maximum length of any route.
      */
     public static final int MAX_ROUTE_LENGTH = ROUTE_CLAIM_POINTS.size() - 1;
 
     /**
-     * Nombre de points bonus obtenus par le(s) joueur(s) disposant du plus long chemin.
+     * Number of bonus points given to the player(s) with the longest trail.
      */
     public static final int LONGEST_TRAIL_BONUS_POINTS = 10;
 }
