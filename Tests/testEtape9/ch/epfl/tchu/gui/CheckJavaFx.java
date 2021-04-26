8a9823ce4016e30458d23b0997fa2a1de2fe7129
Nicolas Cuveillier <Nicolas.cuveillier@epfl.ch>
2021-04-26 15:10:36 +0200

    JFX tested :+1:

---
+++ Tests/testEtape9/ch/epfl/tchu/gui/CheckJavaFx.java
@@ -0,0 +1,14 @@
+package ch.epfl.tchu.gui;
+
+import javafx.scene.paint.Color;
+
+/**
+ * @author Grégory Preisig & Nicolas Cuveillier
+ */
+
+public final class CheckJavaFx {
+    public static void main(String[] args) {
+        Color c = Color.RED;
+        System.out.println(c.getRed());
+    }
+}
