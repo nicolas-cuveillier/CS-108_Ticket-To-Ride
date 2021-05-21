068c14c20231eca061c21b5990ac91de72de1dc4
Chocolat2 <33324447+Chocolat2@users.noreply.github.com>
2021-05-21 14:08:36 +0200

    Update Launcher.java

--- src/ch/epfl/tchu/gui/Launcher.java
+++ src/ch/epfl/tchu/gui/Launcher.java
@@ -2,8 +2,25 @@ package ch.epfl.tchu.gui;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.beans.binding.Bindings;
+import javafx.collections.ObservableList;
+import javafx.geometry.Orientation;
+import javafx.scene.Node;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 public class Launcher extends Application {
+    private final String DEFAULT_HOSTNAME = "localhost";
+    private final int DEFAULT_PORT = 5108;
+    
+    private final int DEFAULT_PLAYERNUMBER = 2;
+    private final String DEFAULT_P1NAME = "Ada";
+    private final String DEFAULT_P2NAME = "Charles";
+    private final String Default_PXNAME = "Player_";
+    
     
     
     public static void main(String[] args) {
@@ -16,4 +33,18 @@ public class Launcher extends Application {
         
     }
 
+}
+
+final class LauncherViewCreator{
+    
+    private LauncherViewCreator() {}
+    
+    public static Node createLauncherView() {
+        VBox launcherView = new VBox();
+        //launcherView.setStyle(null); 
+        
+        
+        return launcherView;
+    }
+    
 }
